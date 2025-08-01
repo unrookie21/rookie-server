bee50dfbcde9b9cef47313f43ee52d6781f799af
rookie <piresj0318@gmail.com>
2025-08-01 20:36:09 +0900

    first commit

---
+++ src/main/java/com/rookie/rookie_server/RookieServerApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.rookie_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookieServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rookie/rookie_server/RookieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.rookie_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookieServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
